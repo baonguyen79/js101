4a62dbc18ebb7a48d88b2db69f1135dfdaafdea0
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-01-24 20:20:16 -0600

    day2.js

---
+++ day2.js
@@ -0,0 +1,18 @@
+var day = "Friday";
+switch (day)
+{
+	case "Monday": 
+		console.log("Booooo");
+		break;
+	case "Friday":
+		console.log("Party");
+		break;
+	default:
+		console.log("not Monday");
+}
+
+
+
+
+
+
